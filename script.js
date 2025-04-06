0e9374970f35a1b609db7229dc2183616d802cb5
Vahe <ohanyanv703@gmail.com>
2025-04-06 13:34:40 +0400

    copy and generate btn

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const copyBtn = document.getElementById("copyBtn");
+const generateBtn = document.getElementById("generateBtn");
+
+
 const updateLength = (value) => {
         document.getElementById("lengthValue").textContent = value;
 }

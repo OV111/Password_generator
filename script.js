ed9e4af174270895b79de8cd130c44b4abd9ed7f
Vahe <ohanyanv703@gmail.com>
2025-04-07 16:37:23 +0400

    simple logic of getting string

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const copyBtn = document.getElementById("copyBtn");
+const generateBtn = document.getElementById("generateBtn");
+
+const includeUppercase = document.getElementById("includeUppercase");
+const includeNumbers = document.getElementById("includeNumbers");
+const includeSymbols = document.getElementById("includeSymbols");
+
+// const passwordLength = document.getElementById("passwordLength");
+
+const passwdLenValue = document.getElementById("lengthValue");
+
 const updateLength = (value) => {
         document.getElementById("lengthValue").textContent = value;
-}
+        return value;
+};
+const getOptions = () => {
+        return {
+                includeNumbers: includeNumbers.checked,
+                includeUppercase: includeUppercase.checked,
+                includeSymbols: includeSymbols.checked,
+        };
+};
+let options = getOptions();
+
+const creatingString = (options) => {
+        let string = "";
+        if(options.includeUppercase) string += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(options.includeNumbers) string += "0123456789";
+        if(options.includeSymbols) string += "!@#$%^&*()_+-=[]{}|;:',.<>?";
+        return string;
+};
+// console.log(creatingString(options));
+
+
+
+
+
+const generatePasswd = (length,pool) => {
+        
+};
+generateBtn.addEventListener("click", () => {
+        console.log(passwdLenValue.textContent);     
+        getOptions();
+        console.log(creatingString(options));
+});

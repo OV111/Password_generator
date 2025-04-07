35ba5db68c6495e33746b3116bad2884932a4f65
Vahe <ohanyanv703@gmail.com>
2025-04-07 22:39:00 +0400

    update Strength

--- script.js
+++ script.js
@@ -3,24 +3,37 @@ const generateBtn = document.getElementById("generateBtn");
 const passwordInput = document.getElementById("password");
 
 const strengthBar = document.getElementById("strengthBar");
-console.log(strengthBar.style.width = "70%");
-
+const strengthLabel = document.getElementById("strengthLabel");
 const includeUppercase = document.getElementById("includeUppercase");
 const includeNumbers = document.getElementById("includeNumbers");
 const includeSymbols = document.getElementById("includeSymbols");
 const passwdLenValue = document.getElementById("lengthValue");
 
 const getOptions = () => {
-    return {
-        includeNumbers: includeNumbers.checked,
-        includeUppercase: includeUppercase.checked,
-        includeSymbols: includeSymbols.checked,
-    };
+        return {
+                includeNumbers: includeNumbers.checked,
+                includeUppercase: includeUppercase.checked,
+                includeSymbols: includeSymbols.checked,
+        };
 };
 const updateLength = (value) => {
         document.getElementById("lengthValue").textContent = value;
         return value;
 };
+const updateStrength = (options) => {
+        if(options.includeUppercase) {
+                strengthBar.style.width = "45%";
+                strengthLabel.textContent = "Medium";
+        }
+        if(options.includeNumbers && options.includeUppercase) {
+                strengthBar.style.width = "70%";
+                strengthLabel.textContent = "High";
+        }
+        if(options.includeNumbers && options.includeUppercase && options.includeSymbols) {
+                strengthBar.style.width = "100%";
+                strengthLabel.textContent = "Very High";
+        }
+}
 const creatingString = (options) => {
         let string = "abcdefghijklmnopqrstuvwxyz";
         if(options.includeNumbers) string += "0123456789";
@@ -40,5 +53,6 @@ generateBtn.addEventListener("click", () => {
         let options = getOptions();
         let len = passwdLenValue.textContent;
         let str = creatingString(options);
+        updateStrength(options);
         passwordInput.value = generatePasswd(len,str);
 });

2b060d46c977cc6b4ca2064054bf5f790bd7d847
Vahe <ohanyanv703@gmail.com>
2025-04-07 17:26:23 +0400

    almost full logic

--- script.js
+++ script.js
@@ -9,14 +9,6 @@ const includeSymbols = document.getElementById("includeSymbols");
 
 const passwdLenValue = document.getElementById("lengthValue");
 
-generateBtn.addEventListener("click", () => {
-        let options = getOptions();
-        console.log(creatingString(options));
-});
-const updateLength = (value) => {
-        document.getElementById("lengthValue").textContent = value;
-        return value;
-};
 const getOptions = () => {
         return {
                 includeNumbers: includeNumbers.checked,
@@ -24,7 +16,11 @@ const getOptions = () => {
                 includeSymbols: includeSymbols.checked,
         };
 };
-// console.log(options.includeNumbers)
+
+const updateLength = (value) => {
+        document.getElementById("lengthValue").textContent = value;
+        return value;
+};
 const creatingString = (options) => {
         let string = "abcdefghijklmnopqrstuvwxyz";
         if(options.includeNumbers) {string += "0123456789";}
@@ -32,7 +28,19 @@ const creatingString = (options) => {
         if(options.includeSymbols) {string += "!@#$%^&*()_+-=[]{}|;:',.<>?";}
         return string;
 };
-
 const generatePasswd = (length,pool) => {
-        
+        let password = "sdw";
+        for(let i = 0; i < length; ++i) {
+            let randomIndex = Math.floor(Math.random() * pool.length);
+            password += pool[randomIndex];
+        }
+        return password;
 };
+
+generateBtn.addEventListener("click", () => {
+        let options = getOptions();
+        // console.log(creatingString(options));
+        let len = passwdLenValue.textContent;
+        let str = creatingString(options)
+        console.log(generatePasswd(len,str));  //!this is the one
+});
